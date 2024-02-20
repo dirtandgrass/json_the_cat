b86f89b6c0a10b08656a0026e017311b01571dfb
dirtandgrass <verdant.luke@gmail.com>
2024-02-20 08:16:51 -0500

    make request create.resolve promise, call it

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,22 @@
 const req = require('request');
 
-req('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
+
+// search thecatapi with the query parameter
+const getBreed = async query => {
+  return new Promise((resolve) => {
+    req(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, (error, response, body) => {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
 
 
-});
+
+      resolve(body);
+    });
+  });
+};
+
+
+(async() => {
+  const data = await getBreed("pers");
+  console.log(data);
+})();
